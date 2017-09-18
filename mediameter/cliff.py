6a3cede97e327ac77e063332056f10efe5528770
Sam Preston <spreston@researchinnovations.com>
2017-09-18 09:41:37 -0400

    updates to support latest CLIFF version and modified /parse/locations endpoint

--- mediameter/cliff.py
+++ mediameter/cliff.py
@@ -1,43 +1,59 @@
 import logging, json
 import requests
 
+def get_url(host, port=None):
+
+    if ':' in host:
+        if port is not None:
+            raise ValueError('cannot specify port in host and separate port')
+        url = host
+    elif port is not None:
+        url =  host + ':' + port
+
+    if not url.startswith('http'):
+        url = 'http://' + url
+
+    return url
+
 class Cliff():
     """Make requests to a CLIFF geo-parsing / NER server"""
 
-    PARSE_TEXT_PATH = "/cliff-2.3.0/parse/text"
-    PARSE_NLP_JSON_PATH = "/cliff-2.3.0/parse/json"
-    PARSE_SENTENCES_PATH = "/cliff-2.3.0/parse/sentences"
-    GEONAMES_LOOKUP_PATH = "/cliff-2.3.0/geonames"
+    CLIFF_BASE_PATH = "/cliff-2.4.0"
 
     JSON_PATH_TO_ABOUT_COUNTRIES = 'results.places.about.countries'
 
-    STATUS_OK = "ok"
-
-    def __init__(self, host, port, text_replacements={}):
+    def __init__(self, host, port=None, text_replacements={}, base_path=CLIFF_BASE_PATH):
         self._log = logging.getLogger(__name__)
-        self._host = host
-        self._port = int(port)
+        self._base_url = get_url(host, port) + base_path
         self._replacements = text_replacements
-        self._log.info("initialized CLIFF @ %s:%d", self._host, self._port)
+        self._log.info("initialized CLIFF @ %s", self._base_url)
+        self._endpoint_text = self.getEndpoint('/parse/text')
+        self._endpoint_sentences = self.getEndpoint('/parse/sentences')
+        self._endpoint_nlp_json = self.getEndpoint('/parse/json')
+        self._endpoint_locations_json = self.getEndpoint('/parse/locations')
+        self._endpoint_geonames = self.getEndpoint('/geonames')
+
+    def getEndpoint(self, path):
+        return self._base_url + path
 
     def parseText(self, text, demonyms=False):
         cleaned_text = self._getReplacedText(text)
-        return self._parseQuery(self.PARSE_TEXT_PATH, cleaned_text , demonyms)
+        return self._parseQuery(self._endpoint_text, cleaned_text , demonyms)
 
     def parseSentences(self, json_object, demonyms=False):
-        return self._parseQuery(self.PARSE_SENTENCES_PATH, json.dumps(json_object), demonyms)
+        return self._parseQuery(self._endpoint_sentences, json.dumps(json_object), demonyms)
 
     def parseNlpJson(self, json_object, demonyms=False):
-        return self._parseQuery(self.PARSE_NLP_JSON_PATH, json.dumps(json_object), demonyms)
+        return self._parseQuery(self._endpoint_nlp_json, json.dumps(json_object), demonyms)
+
+    def parseLocationsJson(self, json_object, demonyms=False):
+        return self._parseQuery(self._endpoint_locations_json, json.dumps(json_object), demonyms)
 
     def geonamesLookup(self, geonames_id):
-        return self._query(self.GEONAMES_LOOKUP_PATH, {'id': geonames_id})['results']
+        return self._query(self._endpoint_geonames, {'id': geonames_id})['results']
 
     def _demonymsText(self, demonyms=False):
         return "true" if demonyms else "false"
-
-    def _urlTo(self, path):
-        return self._host+":"+str(self._port)+path
 
     def _getReplacedText(self, text):
         replaced_text = text
@@ -49,14 +65,12 @@ class Cliff():
         payload = {'q': text, 'replaceAllDemonyms': self._demonymsText(demonyms)}
         self._log.debug("Querying %r (demonyms=%r)", path, demonyms)
         return self._query(path,payload)
-    
-    def _query(self, path, args):
+
+    def _query(self, url, args):
         try:
-            url = self._urlTo(path)
             r = requests.post(url, data=args)
             self._log.debug('CLIFF says %r', r.content)
             return r.json()
         except requests.exceptions.RequestException as e:
             self._log.exception(e)
         return ""
-
